86b545638f67383bb09e5ce180e8939407449713
Tayane Cristina Alves dos Santos <tay-cristinarj@hotmail.com>
2023-05-19 13:42:20 -0300

    second commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// Escreva uma série de váriaveis com suas informações sobre: (nome, sobrenome, idade, peso, altura):
+
+const nome = "Tayane Cristina"
+const sobrenome = "Alves dos Santos"
+const idade = "22"
+const peso = "53"
+const altura = "1.59"
+
+console.log("A estudante" , nome, sobrenome, "de,", idade, "anos, com ", altura, "de altura, pesa", peso,"quilos.")
